19ebcc938ab6131fdcf16f549ab098841fd47dc4
Sphesihle <dsihledlamini10@gmail.com>
2023-03-28 08:15:35 -0700

    Sihle

--- main.h
+++ main.h
@@ -1,62 +1,81 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+#define UNUSED(x) (void)(x)
+
+#define BUFF_SIZE 1024
+
+
+
+/* FLAGS */
+
+#define F_MINUS 1
+
+#define F_PLUS 2
+
+#define F_ZERO 4
+
+#define F_HASH 8
+
+#define F_SPACE 16
+
+
+
+/* SIZES */
+
+#define S_LONG 2
+
+#define S_SHORT 1
+
 
 
 /**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
  */
 
-typedef struct specifiers
+struct fmt
+
 {
-	char specifiers;
-	int (*f)(va_list);
-} specifiers_t;
 
-/*prototypes*/
+char fmt;
+
+int (*fn)(va_list, char[], int, int, int, int);
+
+};
+
+
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+
+typedef struct fmt fmt_t;
+
+
+
 int _printf(const char *format, ...);
-int get_function(char s, va_list args);
-int _putchar(char c);
 
-/*Conversion specifiers*/
-int print_char(va_list args);
-int print_string(va_list args);
-int print_digit(va_list args);
-int print_mod(va_list args);
-int print_rev_string(va_list args);
+int handle_print(const char *fmt, int *i,
 
-/*utils*/
-int is_printable(char);
-int append_hexa_code(char, char[], int);
-int is_digit(char);
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
-
-/* width handler */
-int handle_write_char(char c, char buffer[],
-int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-
-int length, char padd, char extra_c);
-
-int write_pointer(char buffer[], int ind, int length,
-
-int width, int flags, char padd, char extra_c, int padd_start);
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
-
-int write_unsgnd(int is_negative, int ind,
-
-char buffer[],
-
-int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
@@ -154,5 +173,50 @@ int print_rot13string(va_list types, char buffer[],
 
 int flags, int width, int precision, int size);
 
-#endif
 
+
+/* width handler */
+
+int handle_write_char(char c, char buffer[],
+
+int flags, int width, int precision, int size);
+
+int write_number(int is_positive, int ind, char buffer[],
+
+int flags, int width, int precision, int size);
+
+int write_num(int ind, char bff[], int flags, int width, int precision,
+
+int length, char padd, char extra_c);
+
+int write_pointer(char buffer[], int ind, int length,
+
+int width, int flags, char padd, char extra_c, int padd_start);
+
+
+
+int write_unsgnd(int is_negative, int ind,
+
+char buffer[],
+
+int flags, int width, int precision, int size);
+
+
+
+/****************** UTILS ******************/
+
+int is_printable(char);
+
+int append_hexa_code(char, char[], int);
+
+int is_digit(char);
+
+
+
+long int convert_size_number(long int num, int size);
+
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+
+
+#endif /* MAIN_H */
